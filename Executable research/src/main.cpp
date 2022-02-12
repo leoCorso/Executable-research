792f7acec8346a37c088112a29523c19984adb30
leoCodes <leoCodes@outlook.com>
2022-02-12 12:21:30 -0800

    Add project files.

---
+++ Executable research/src/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+#include <string>
+
+#include <filesystem>
+
+
+
+int main() 
+{
+	std::string input;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << ".>";
+	std::cin >> input;
+	
+
+	return 0;
+}
